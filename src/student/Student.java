3435db82994b0dab4b9e9edfcecf7cded0c51845
Jonathan Persaud <persajon@sheridancollege.ca>
2022-01-21 09:38:12 -0500

    first commit

---
+++ src/student/Student.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jonathan
+ */
+public class Student {
+ private String studentId;
+ private String studentName;
+    
+  
+ 
+ /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+/**        char [] myWorld = new char[7];
+       myWorld[0]='s';
+        myWorld[1]='t';
+        myWorld[2]='u';
+        myWorld[3]='d';
+        myWorld[4]='e';
+        myWorld[5]='n';
+        myWorld[6]='t';
+
+        for(int i=0;i<myWorld.length;i++){
+            System.out.print(myWorld[i]);
+        }
+        System.out.println();
+        for(int i=myWorld.length-1;i>=0;i--){
+            System.out.print(myWorld[i]);
+        } */
+      /**  Scanner sc= new Scanner(System.in);
+        System.out.println("Please enter a word");
+        String word =sc.nextLine();
+        char[] myLetters =new char[word.length()];
+        for(int i=0;i<myLetters.length;i++){
+            myLetters[i]=word.charAt(i);
+            System.out.println(myLetters[i]);
+        } */
+      
+     
+      
+    }
+    
+}
---
+++ src/student/Studentlist.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author jonathan
+ */
+public class Studentlist {
+    
+     public static void main(String[] args) {
+         
+         Student[] Studentlist= new Student[3];
+         
+         Studentlist[0] = new Student ("hello","Kid","Kid2");
+         
+         
+     }
+}
